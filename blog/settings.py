2f01026709c8375b40441a3c82e85af313184128
gello94 <gello94@live.it>
2019-09-02 17:43:10 +0100

    missing psycopg2 installed

--- blog/settings.py
+++ blog/settings.py
@@ -86,11 +86,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if "DATABASE_URL" is os.environ:
+if "DATABASE_URL" in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
+    print("Postgres URL not found, using sqlite instead")
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
