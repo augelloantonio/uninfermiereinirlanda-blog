b3e4783c009193a29e1a07599295862010e81c27
gello94 <gello94@live.it>
2019-11-20 16:55:42 +0000

    added author to post model and added index page

--- posts/models.py
+++ posts/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     """
     A single Blog post
     """
+    author = models.ForeignKey(User, unique=False,
+                               on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=200)
     content = models.CharField(max_length=100000)
     created_date = models.DateTimeField(auto_now_add=True)
