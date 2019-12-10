00f92063afe3fd38bd2a3cd1a3588be69e5207ca
gello94 <gello94@live.it>
2019-12-10 19:56:00 +0000

    update js file

--- static/js/custom.js
+++ static/js/custom.js
@@ -20,3 +20,16 @@ function sendMail(contactForm) {
 }
 
 
+//NavBar scroll
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    if (window.pageYOffset >= 60) {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("navbar").style.top = "";
+        } else {
+            document.getElementById("navbar").style.top = "-60px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+};
